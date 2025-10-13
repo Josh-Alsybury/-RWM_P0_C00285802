28a9058b3d42ddc71ca6bfea29155c0d789fc7ef
unknown <jalsybury@gmail.com>
2025-10-13 10:59:55 +0100

    Use Math.pow and explicit mapper

--- Sprint 7/src/lib/filters/CubeFilter_Base.ts
+++ Sprint 7/src/lib/filters/CubeFilter_Base.ts
@@ -1,8 +1,8 @@
 export function cube(x: number): number {
-  return x * x * x;
+  return Math.pow(x, 3)
 }
 
 // Map over a sequence
 export function cubes(xs: number[]): number[] {
-  return xs.map(cube);
+  return xs.map((value) => cube(value));
 }
